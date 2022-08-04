44526c8c97d667fb06345dbeed17de5624c965dc
Timothy <lemayiant4@gmail.com>
2022-08-04 20:14:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    const cardContent=[];
+    for (let i = 0; i<names.length; i++) {
+        cardContent.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`,);
+    }
+    return cardContent
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+// function countdown(no) { 
+//     let number= 0
+//     while (number < no ) {
+//     console.log(no-1);
+//     number--
+//     }
+// }
+
+// let countup = 0;
+// while (countup < 10) {
+//   console.log(countup++);
+// }
+function countDown() {
+    let countdown = 10 ;
+    while (countdown >= 0) {
+    console.log(countdown--);
+}
+}
